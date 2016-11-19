b056d1360de5807b2b4f61c3d9ab2d2886cbf4bc
MSPRO <maxime.sifflet@gmail.com>
2016-11-19 14:17:45 +0100

    Update work directory

--- alpine/startup.sh
+++ alpine/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d /src/mysql ]; then
+if [ -d /app/mysql ]; then
   echo "[i] MySQL directory already present, skipping creation"
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
